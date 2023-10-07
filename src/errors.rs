f4d2f88c7dd58170c3fefcc0b4e7464c6c832dad
h3rt <alexander.hertlein@icloud.com>
2023-10-08 00:41:15 +0200

    clean

--- src/errors.rs
+++ src/errors.rs
@@ -6,7 +6,4 @@ pub enum CustomContractError {
     #[error("{0}")]
     // let thiserror implement From<StdError> for you
     Std(#[from] StdError),
-    // this is whatever we want
-    #[error("Custom error #1: Game cannot be found")]
-    GameNotFound,
 }
